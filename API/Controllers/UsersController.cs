38252ca1666a3ebc26896bfa681c1fb91c74eef5
Xinan <youkingan11@gmail.com>
2024-03-20 15:02:38 +0000

    updated gitignore

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,11 +1,12 @@
 ﻿using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
 [ApiController]
-[Route("api/[controller]")]     // /api/users
+[Route("api/[controller]")]     // https://localhost:5001/api/users
 
 public class UsersController : ControllerBase
 {
@@ -17,16 +18,16 @@ public class UsersController : ControllerBase
     }
 
     [HttpGet]
-    public ActionResult<IEnumerable<AppUser>> GetUsers()
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
-        var users = _context.Users.ToList();
+        var users = await _context.Users.ToListAsync();
 
         return users;
     }
 
-    [HttpGet("{id}")]       //  /api/users/2
-    public ActionResult<AppUser> GetUser(int id)
+    [HttpGet("{id}")]       // https://localhost:5001/api/users/2
+    public async Task<ActionResult<AppUser>> GetUser(int id)
     {
-        return _context.Users.Find(id);
+        return await _context.Users.FindAsync(id);
     }
 }
