a2e9aea76e5220b661f62e0ae7097d34c432e2fb
Xinan <youkingan11@gmail.com>
2024-03-20 14:45:49 +0000

    s2 - 16

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]     // /api/users
+
+public class UsersController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<AppUser>> GetUsers()
+    {
+        var users = _context.Users.ToList();
+
+        return users;
+    }
+
+    [HttpGet("{id}")]       //  /api/users/2
+    public ActionResult<AppUser> GetUser(int id)
+    {
+        return _context.Users.Find(id);
+    }
+}
--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Data;
 
 public class DataContext : DbContext
 {
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
 
+    public DbSet<AppUser> Users { get; set; }
 }
