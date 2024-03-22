b4218f26ff1a8b0e341a10294b89658e06d9b303
Xinan <youkingan11@gmail.com>
2024-03-22 14:22:40 +0000

    section3 complete

--- API/Program.cs
+++ API/Program.cs
@@ -17,7 +17,7 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
