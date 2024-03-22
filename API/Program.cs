c22219359609d6b7acd8ac40fb50a5433483975c
Xinan <youkingan11@gmail.com>
2024-03-22 14:07:24 +0000

    add HTTP request, CORS, bootstrap

--- API/Program.cs
+++ API/Program.cs
@@ -11,13 +11,13 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.MapControllers();
 
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  constructor() {
-    console.log('App component created');
-  }
+export class AppComponent implements OnInit{
   title = 'Dating app is running!';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit():void {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: response => this.users = response,
+      error: error => console.log(error),
+      complete: () => console.log('request completed')
+    })
+  }
 }
