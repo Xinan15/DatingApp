d09cd400d669b8ebd20eacd43ed713c0897703c3
Xinan <youkingan11@gmail.com>
2024-03-25 14:15:42 +0000

    add authentication section

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using System.Text;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,19 +13,22 @@ public class AccountController : BaseApiController
 {
     private readonly DataContext _context;
 
-    public AccountController(DataContext context)
+    private readonly ITokenService _tokenService;
+
+    public AccountController(DataContext context, ITokenService tokenService)
     {
+        _tokenService = tokenService;
         _context = context;
     }
 
     [HttpPost("register")]    // https://localhost:5001/api/account/register
 
-    public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+    public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
     {
         if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
 
         using var hmac = new HMACSHA512();
-        
+
         var user = new AppUser
         {
             UserName = registerDto.Username.ToLower(),
@@ -33,10 +37,30 @@ public class AccountController : BaseApiController
         };
 
         _context.Users.Add(user);
-
         await _context.SaveChangesAsync();
 
-        return user;
+        return new UserDto{
+            Username = user.UserName,
+            Token = _tokenService.CreateToken(user)
+        };
+    }
+
+    [HttpPost("login")]    // https://localhost:5001/api/account/login
+    public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+    {
+        var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+        if (user == null) return Unauthorized("Invalid username");
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+        var ComputeHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+        
+        for (int i = 0; i < ComputeHash.Length; i++)
+        {
+            if (ComputeHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+        }
+        return new UserDto{
+            Username = user.UserName,
+            Token = _tokenService.CreateToken(user)
+        };
     }
 
     private async Task<bool> UserExists(string username)
--- API/Program.cs
+++ API/Program.cs
@@ -1,23 +1,24 @@
-using API.Data;
-using Microsoft.EntityFrameworkCore;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-builder.Services.AddCors();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIndentityServices(builder.Configuration);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
+// add the authorization middleware before the map controllers middleware
+
+app.UseAuthentication();    // asking: do you have an authorised token? / valid ID?
+
+app.UseAuthorization();      // asking: do you have the right to access this resource? / enough age for the nightclub?
 
 app.MapControllers();
 
