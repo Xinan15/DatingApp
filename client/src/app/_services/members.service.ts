d866343202a857c7b912f61514e73d5c6df6204e
Xinan <youkingan11@gmail.com>
2024-04-24 15:13:18 +0100

    add feature: user likes

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -18,12 +18,12 @@ export class MembersService {
   user?: User | null;
   userParams: UserParams | undefined;
 
-  constructor(private http: HttpClient, private accountService: AccountService) { 
+  constructor(private http: HttpClient, private accountService: AccountService) {
     this.accountService.currentUser$.pipe(take(1)).subscribe({
       next: user => {
         if (user)
           this.userParams = new UserParams(user);
-          this.user = user;
+        this.user = user;
       }
     })
   }
@@ -70,7 +70,7 @@ export class MembersService {
       .find((member: Member) => member.userName === username);
 
     if (member) return of(member);
-    
+
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 
@@ -89,6 +89,18 @@ export class MembersService {
 
   deletePhoto(photoId: number) {
     return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
+  }
+
+  addLike(username: string) {
+    return this.http.post(this.baseUrl + 'likes/' + username, {})
+  }
+
+  getLikes(predicate: string, pageNumber: number, pageSize: number) {
+    let params = this.getPaginationHeaders(pageNumber, pageSize);
+
+    params = params.append('predicate', predicate);
+
+    return this.getPaginatedResult<Member[]>(this.baseUrl + 'likes', params);
   }
 
   private getPaginatedResult<T>(url: string, params: HttpParams) {
